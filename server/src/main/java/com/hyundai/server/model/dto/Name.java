fcac8dbfbf74a8d3d9311d8711015a7672f17ebb
JunSeong <gentlmanjun@naver.com>
2023-06-28 10:12:25 +0900

    [SERVER] feat | impl name dto VCGT-5

---
+++ server/src/main/java/com/hyundai/server/model/dto/Name.java
@@ -0,0 +1,17 @@
+package com.hyundai.server.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Name {
+    private int id;
+    private String name;
+    private String car_type;
+    private boolean is_new;
+    private int start_price;
+    private String car_image;
+}
