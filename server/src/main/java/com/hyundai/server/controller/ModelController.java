407e97eb481a2a33106b6bb89f52c1fe6204db86
JunSeong <gentlmanjun@naver.com>
2023-07-02 20:54:28 +0900

    [SERVER] feat | impl powertrain change VCGT-16

--- server/src/main/java/com/hyundai/server/controller/ModelController.java
+++ server/src/main/java/com/hyundai/server/controller/ModelController.java
@@ -1,6 +1,8 @@
 package com.hyundai.server.controller;
 
+import com.hyundai.server.common.request.ChangePowertrainReq;
 import com.hyundai.server.common.response.BaseResponseBody;
+import com.hyundai.server.common.response.ChangePowertrainRes;
 import com.hyundai.server.common.response.PowertrainOptionListRes;
 import com.hyundai.server.model.dto.PowertrainDto;
 import com.hyundai.server.model.service.ModelService;
@@ -22,14 +24,37 @@ public class ModelController {
     @ApiImplicitParam(name = "car_name_id", value = "차종 id")
     public ResponseEntity<? extends BaseResponseBody> showPowertrainOptionList(@RequestParam("car_name_id") Integer carNameId) {
         try{
-            List<String> engine = modelService.showEngine(carNameId);
-            List<String> transmission = modelService.showTransmission(carNameId);
-            List<String> drivetrain = modelService.showDrivetrain(carNameId);
+            List<String> engine = modelService.getEngineList(carNameId);
+            List<String> transmission = modelService.getTransmissionList(carNameId);
+            List<String> drivetrain = modelService.getDrivetrainList(carNameId);
 
             return ResponseEntity.ok(PowertrainOptionListRes.of(200, "success", engine, transmission, drivetrain));
         }catch (RuntimeException e){
             return ResponseEntity.status(500)
                     .body(BaseResponseBody.of(500, e.getMessage()));
+        }
+    }
+
+    @PostMapping("/powertrain")
+//    @ApiImplicitParam(name = "car_name_id", value = "차종 id")
+    public ResponseEntity<? extends BaseResponseBody> changePowertrain(ChangePowertrainReq changePowertrainReq) {
+        try{
+//            carNameId가 null이면 에러
+            if(changePowertrainReq.getCarNameId() == null)
+                throw new Exception("car name id가 없습니다.");
+
+//            파워트레인 변경 결과 출력
+            PowertrainDto powertrain = modelService.getChangedPowertrian(changePowertrainReq.getCarNameId(), changePowertrainReq.getEngine(), changePowertrainReq.getTransmission(), changePowertrainReq.getDrivetrain());
+            if(powertrain == null)
+                throw new Exception("주어진 파워트레인 값에 해당하는 결과가 없습니다.");
+
+            return ResponseEntity.ok(ChangePowertrainRes.of(200, "success", powertrain));
+        } catch (RuntimeException e){
+            return ResponseEntity.status(500)
+                    .body(BaseResponseBody.of(500, e.getMessage()));
+        } catch (Exception e) {
+            return ResponseEntity.status(400)
+                    .body(BaseResponseBody.of(400, e.getMessage()));
         }
     }
 }
