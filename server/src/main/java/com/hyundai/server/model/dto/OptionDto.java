f9ceb43c12afc44f15112afdfa61f35650f8bc05
JunSeong <gentlmanjun@naver.com>
2023-07-04 22:57:33 +0900

    [SERVER] feat | impl OptionDto VCGT-21

---
+++ server/src/main/java/com/hyundai/server/model/dto/OptionDto.java
@@ -0,0 +1,18 @@
+package com.hyundai.server.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OptionDto {
+    private int id;
+    private int price;
+    private String type;
+    private String name;
+    private String image;
+    private String preview;
+    private Boolean available;
+}
