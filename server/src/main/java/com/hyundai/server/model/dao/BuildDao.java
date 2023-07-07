b779c8b0573e9c992f839e0eea5cd9a9bed323c3
JunSeong <gentlmanjun@naver.com>
2023-07-07 15:00:57 +0900

    [SERVER] fix | fix BuildDao VCGT-22

--- server/src/main/java/com/hyundai/server/model/dao/BuildDao.java
+++ server/src/main/java/com/hyundai/server/model/dao/BuildDao.java
@@ -3,19 +3,26 @@ package com.hyundai.server.model.dao;
 import com.hyundai.server.model.dto.OptionDto;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Repository
 public interface BuildDao {
+    OptionDto selectOptionByOptionId(int optionId);
 //    Color
+//    차종에서 선택 가능한 모든 색상
     List<OptionDto> selectInteriorByCarNameId(int carNameId);
     List<OptionDto> selectExteriorByCarNameId(int carNameId);
+//    모델에서 사용 가능한 색상
     List<OptionDto> selectInteriorByModelId(int modelId);
     List<OptionDto> selectExteriorByModelId(int modelId);
-    List<OptionDto> selectAvailableInterior(int exteriorOptionId);
-    List<OptionDto> selectAvailableExterior(int interiorOptionId);
+//    해당 차종에서 사용가능한 모든 색상 조합
+//    interiorId(exteriorId), carNameId
+    List<OptionDto> selectAvailableInterior(HashMap<String, Object> map);
+    List<OptionDto> selectAvailableExterior(HashMap<String, Object> map);
 //    Option
-    List<OptionDto> selectRequiredOption(int optionId);
-    List<OptionDto> selectExclusiveOption(int optionId);
-    List<OptionDto> selectAvailableOption(int optionId);
+//    optionId, modelId
+    List<OptionDto> selectRequiredOption(HashMap<String, Object> map);
+    List<OptionDto> selectExclusiveOption(HashMap<String, Object> map);
+    List<OptionDto> selectAvailableOption(HashMap<String, Object> map);
 }
