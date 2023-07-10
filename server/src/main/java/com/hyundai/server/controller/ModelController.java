055769f8dcb2b997a6b7b3f93ad39273018fff30
JunSeong <gentlmanjun@naver.com>
2023-07-11 02:00:57 +0900

    [SERVER] modify | add 'api' at url VCGT-72

--- server/src/main/java/com/hyundai/server/controller/ModelController.java
+++ server/src/main/java/com/hyundai/server/controller/ModelController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/models")
+@RequestMapping("api/models")
 public class ModelController {
 
     @Autowired
