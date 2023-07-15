afe834aa4f30ef7f1905b69b3ecadf653243394b
JunSeong <gentlmanjun@naver.com>
2023-07-15 16:24:39 +0900

    [CLIENT] feat | impl exit button VCGT-43

--- client/src/components/buildModel/Header/BuildHeader.tsx
+++ client/src/components/buildModel/Header/BuildHeader.tsx
@@ -1,9 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, useSearchParams } from 'react-router-dom';
+import { Link, useNavigate, useSearchParams } from 'react-router-dom';
 import { BuildDropdown } from './BuildDropdown';
-import { toggleBuildDropdown } from '../../../slice/modelSlice';
+import { openModal, toggleBuildDropdown } from '../../../slice/modelSlice';
 import { BuildNav } from './BuildNav';
 import * as style from '../../../styles/buildModel/buildNavBarStyle';
+
+function message() {
+  return <div>내 차 만들기를 종료하시겠습니까?</div>;
+}
 
 export function BuildHeader() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -13,6 +17,19 @@ export function BuildHeader() {
   const showDropdown = useSelector((state: any) => state.powertrain.showBuildDropdown);
   const toggle = () => {
     dispatch(toggleBuildDropdown());
+  };
+
+  // 종료 모달
+  const navigate = useNavigate();
+
+  const children = message();
+  const onCancel = () => {};
+  const onSubmit = () => {
+    navigate(`/`);
+  };
+
+  const modal = () => {
+    dispatch(openModal({ children, onCancel, onSubmit }));
   };
 
   return (
@@ -37,7 +54,11 @@ export function BuildHeader() {
               </button>
             </div>
           </style.UpperLeft>
-          <div>exit</div>
+          <div>
+            <button onClick={modal} type="button">
+              종료
+            </button>
+          </div>
         </style.UpperHeader>
         <style.UnderHeader>
           <BuildNav />
