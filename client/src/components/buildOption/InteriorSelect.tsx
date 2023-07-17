ab026fe513a1e422734ee7e539586c2fd9435bc4
JunSeong <gentlmanjun@naver.com>
2023-07-17 15:54:40 +0900

    [CLIENT] feat | impl interior tile VCGT-59

---
+++ client/src/components/buildOption/InteriorSelect.tsx
@@ -0,0 +1,53 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
+import { AppDispatch } from '../../store/store';
+import { fetchInterior, setNextInterior } from '../../slice/buildSlice';
+import { Color } from '../../types/sliceType';
+import { InteriorTile } from './InteriorTile';
+import * as style from '../../styles/buildOption/colorStyle';
+
+export function InteriorSelect() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const id = Number(searchParams.get('id'));
+  const modelId = Number(searchParams.get('modelId'));
+
+  const dispatch = useDispatch<AppDispatch>();
+  const nextInteriorId = useSelector((state: any) => state.build.nextInteriorId);
+  const nextExteriorId = useSelector((state: any) => state.build.nextExteriorId);
+  const interiorList = useSelector((state: any) => state.build.interiorList);
+
+  const refreshInterior = () => {
+    const payload = {
+      carNameId: id,
+      modelId,
+      exteriorId: nextExteriorId,
+    };
+    dispatch(fetchInterior(payload));
+  };
+
+  useEffect(() => {
+    refreshInterior();
+  }, [nextExteriorId]);
+
+  useEffect(() => {
+    const found = interiorList.find((e: Color) => e.optionId == nextInteriorId);
+    if (!found?.available) {
+      for (const interior of interiorList) {
+        if (interior.available) {
+          dispatch(setNextInterior(interior.optionId));
+          break;
+        }
+      }
+    }
+  }, [interiorList]);
+
+  return (
+    <>
+      <style.ColorTitle>내장색상</style.ColorTitle>
+      <div>
+        <ul>{interiorList?.map((interior: Color) => <InteriorTile interior={interior} />)}</ul>
+      </div>
+    </>
+  );
+}
---
+++ client/src/components/buildOption/InteriorTile.tsx
@@ -0,0 +1,46 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
+import { Color } from '../../types/sliceType';
+import { AppDispatch } from '../../store/store';
+import { checkExIn, setNextInterior } from '../../slice/buildSlice';
+import { ColorBtn } from '../common/ColorBtn';
+import * as style from '../../styles/buildOption/colorStyle';
+
+export function InteriorTile({ interior }: { interior: Color }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const id = Number(searchParams.get('id'));
+  const modelId = Number(searchParams.get('modelId'));
+
+  const dispatch = useDispatch<AppDispatch>();
+  const oldInteriorId = useSelector((state: any) => state.build.interiorId);
+  const oldExteriorId = useSelector((state: any) => state.build.exteriorId);
+
+  const changeColor = (interiorId: number, available: boolean) => {
+    if (available) {
+      dispatch(setNextInterior(interiorId));
+    } else {
+      const paylaod = {
+        beforeEx: oldExteriorId,
+        beforeIn: oldInteriorId,
+        carNameId: id,
+        exterior: oldExteriorId,
+        interior: interiorId,
+        modelId,
+      };
+      dispatch(checkExIn(paylaod));
+    }
+  };
+
+  return (
+    <style.ColorTile>
+      <ColorBtn
+        name={interior.name}
+        available={interior.available}
+        price={null}
+        selected={oldInteriorId === interior.optionId}
+        func={() => changeColor(interior.optionId, interior.available)}>
+        <style.InteriorImg src={`${interior.image}`} alt="interiorImage" />
+      </ColorBtn>
+    </style.ColorTile>
+  );
+}
