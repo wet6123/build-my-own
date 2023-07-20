96f993e71d1121508c7a8574be0eaa8fbdf2d7f8
JunSeong <gentlmanjun@naver.com>
2023-07-21 03:23:23 +0900

    [SERVER] modify | modify powertrain api response VCGT-79

--- server/src/main/java/com/hyundai/server/common/response/ChangePowertrainRes.java
+++ server/src/main/java/com/hyundai/server/common/response/ChangePowertrainRes.java
@@ -7,13 +7,17 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ChangePowertrainRes extends BaseResponseBody{
-    private PowertrainDto changedPowertrain;
+    private String engine;
+    private String transmission;
+    private String drivetrain;
 
-    public static ChangePowertrainRes of (int statusCode, String message, PowertrainDto changedPowertrain) {
+    public static ChangePowertrainRes of (int statusCode, String message, PowertrainDto powertrain) {
         ChangePowertrainRes res = new ChangePowertrainRes();
         res.setStatusCode(statusCode);
         res.setMessage(message);
-        res.setChangedPowertrain(changedPowertrain);
+        res.setEngine(powertrain.getEngine());
+        res.setTransmission(powertrain.getTransmission());
+        res.setDrivetrain(powertrain.getDrivetrain());
         return res;
     }
 }
